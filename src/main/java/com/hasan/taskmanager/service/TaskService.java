c70a9b33fe866c43ce510d9864e9dc8951d306f3
Hasan Yilmaz <mailin@gmail.com>
2026-04-23 21:13:45 +0200

    feat: implement task service

---
+++ src/main/java/com/hasan/taskmanager/service/TaskService.java
@@ -0,0 +1,29 @@
+package com.hasan.taskmanager.service;
+
+import com.hasan.taskmanager.entity.Task;
+import com.hasan.taskmanager.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
