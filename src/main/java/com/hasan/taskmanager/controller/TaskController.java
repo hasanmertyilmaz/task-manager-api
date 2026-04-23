98401f7acde76b729202b410b25031a5dd10e9ec
Hasan Yilmaz <mailin@gmail.com>
2026-04-23 21:37:49 +0200

    feat: add task controller endpoints

---
+++ src/main/java/com/hasan/taskmanager/controller/TaskController.java
@@ -0,0 +1,33 @@
+package com.hasan.taskmanager.controller;
+
+import com.hasan.taskmanager.entity.Task;
+import com.hasan.taskmanager.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+}
